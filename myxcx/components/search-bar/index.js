f79d0c961ba9483fb3cfef8f30cf18fa3a220dbd
xukaishan <369237387@qq.com>
2019-12-06 17:35:16 +0800

    xcx

---
+++ myxcx/components/search-bar/index.js
@@ -0,0 +1,47 @@
+// components/search-bar/index.js
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    inputShowed: false,
+    inputVal: ""
+  },
+  
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    showInput: function () {
+      this.setData({
+        inputShowed: true
+      });
+    },
+    hideInput: function () {
+      this.setData({
+        inputVal: "",
+        inputShowed: false
+      });
+    },
+    clearInput: function () {
+      this.setData({
+        inputVal: ""
+      });
+    },
+    inputTyping: function (e) {
+      this.setData({
+        inputVal: e.detail.value
+      });
+    }
+  }
+})
