f8f4c07958f7b0513c692e88198195d6328ec6b6
xukaishan <44866370+xukaishan@users.noreply.github.com>
2020-02-09 16:38:38 +0800

    Update config.js

--- xyp/config.js
+++ xyp/config.js
@@ -7,27 +7,10 @@ var config = {
   //正式环境
 
   //Linux服务器
-  ImageDomainURL: "https://daogouapi.youxuepai.com/",
-  LNTUOEDomain: "https://daogouapi.youxuepai.com/",
+  ImageDomainURL: "https://192.168.xx.x/",
+  LNTUOEDomain: "https://192.168.xx.x/",
 
-  //Windows服务器
-  // ImageDomainURL: "https://daogouapi.youxuepai.com/",
-  // LNTUOEDomain: "https://daogouapi.youxuepai.com/api2.0/",
-
-  //测试环境
-  // ImageDomainURL: "https://api.test.gviiii.com/",
-  // LNTUOEDomain: "https://api.test.gviiii.com/",
-
-  //本都环境
-  // ImageDomainURL: "http://localhost:8082/",
-  // LNTUOEDomain: "http://localhost:8082/", 
-
-  //(1.7.7 API - Windows服务器）
-  //(1.7.8 API - Linux服务器）
-  //(1.7.9 API - Windows服务器）
-  //(1.8.0 API - Linux服务器）
-  //(1.8.1 API - Windows服务器）
-  //(1.8.2 API - Linux服务器）
+  
 
   APPNUM: "1.8.2"//版本号
   
